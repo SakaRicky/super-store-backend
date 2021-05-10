b5ee6681146014a8f7c9dd0312068bc8858a31dd
Saka Ricky <sakaricky91@gmail.com>
2021-05-10 18:52:47 +0100

    Change the way to query the backend, now it uses the params in the request query

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path');
+
 
 const middleware = require('./utils/middleware')
 const itemsRouter = require('./controllers/items')
@@ -8,9 +10,12 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
 app.use(middleware.requestLogger)
 
 app.use('/item', itemsRouter)
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(middleware.unknownEndpoint)
 
--- controllers/items.js
+++ controllers/items.js
@@ -5,43 +5,76 @@ const path = require('path')
 
 const logger = require('../utils/logger')
 
-itemsRouter.get('/list', async (request, response, error) => {
-    const items = await itemModel.find({})
-    response.send({items : items})
+itemsRouter.get('/list', async (req, res, error) => {
+
+    const params = req.query
+
+    console.log('params', params);
+
+    if (params.q) {
+        const queriedItems = await itemModel.find({name: params.q})
+        res.send({items : queriedItems})
+    }
+
+    let items = null
+
+    if (params.isOnSale) {
+        items = await itemModel.find({isOnSale: params.isOnSale}).skip(parseInt(params.from)).limit(parseInt(params.size))
+    } else {
+        items = await itemModel.find({}).skip(parseInt(params.from)).limit(parseInt(params.size))
+    }
+
+    res.send({items : items})
 })
 
-itemsRouter.post('/', async (request, response, error) => {
-    const item_received = request.body
+itemsRouter.get('/:id', async (req, res, error) => {
+    const id = req.params.id
+    const item = await itemModel.findById(id)
+    res.send(item)
+})
+
+itemsRouter.post('/', async (req, res, error) => {
+    const item_received = req.body
     logger.info(item_received);
 
     const item = new itemModel(item_received)
 
     const saved_item = await item.save()
 
-    response.send(saved_item)
+    res.send(saved_item)
 
 })
 
 const storage = multer.diskStorage({
-    destination: 'images',
+    destination: 'public/images',
     filename: function(req, file, cb){
-        cb(null, file.filename + '-' + Date.now() + path.extname(file.originalname))
+        cb(null, file.originalname + '-' + Date.now() + path.extname(file.originalname))
     }
 })
 
 //multer options
 const upload = multer({
     storage: storage
-}).single('upload')
+}).single('image')
 
-itemsRouter.post('/upload', (req, res) => {
-    console.log('uploading image');
-    console.log(req.file);
-    upload(req, res, (err) => {
-        console.log('req.file', req.file);
-        console.log('req.body', req.body);
+itemsRouter.post('/upload', async (req, res) => {
+    upload(req, res, async (err) => {
+
+        const item_received = req.body
+        logger.info(item_received);
+
+        const image_path = req.file.path
+        logger.info('req.file', req.file)
+        item_received.imageUrl = `http://localhost:5000/${image_path.slice(7)}`
+        logger.info(item_received)
+
+        const item = new itemModel(item_received)
+
+        const saved_item = await item.save()
+
+        res.send(saved_item)
     })
-    res.send()
+    // res.send()
 })
 
 module.exports = itemsRouter
