6a9bb5498aead990adb8c9df5ce2d54b5cfd8091
Saka Ricky <sakaricky91@gmail.com>
2021-10-11 16:55:05 +0100

    uibuild

--- utils/config.js
+++ utils/config.js
@@ -9,6 +9,7 @@ const MONGODB_URI = process.env.NODE_ENV === 'test'
     : process.env.MONGODB_URI
 
 console.log('MONGODB_URI', MONGODB_URI);
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
 module.exports = {
     MONGODB_URI,
